a03ddff286e701577e289ded1f0e8ffa4ea3e072
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-12-23 12:25:47 -0700

    commit tree 223c8ace543a56e5b2d26e98693eff58a63b1f52

--- index.js
+++ index.js
@@ -10,4 +10,15 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+  const time = timeString.split(':')
+  if (time[0] < 0 || time[0] > 24) return "Not a valid time"
+  if (time[0] < 12) return "Good Morning"
+  if (time[0] > 17) return "Good Evening"
+  return "Good Afternoon"
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(message) {
+  document.getElementById("greeting").textContent = message
+}
